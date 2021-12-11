0bd8e7ad60954e192febfb0ba7a1e2d7a4e36817
Mauriciorpx <mauricio3741@gmail.com>
2021-12-10 23:06:33 -0500

    Portafolio ingles V1.0

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
-/* addEventListener('DOMContentLoaded', () =>{
-    const btn_menu = document.querySelector('.btn-menu')
-    if(btn_menu){
-        btn_menu.addEventListener('click', ()=>{
-            const menu_items = document.querySelector('.menu-items')
-            menu_items.classList.toggle('show')
-        })
-    }
-}) */
+const hamburguer = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-const ham = document.querySelector('.ham');
-const enlaces = document.querySelector('.enlaces-menu');
-const barras = document.querySelectorAll('.ham span');
+hamburguer.addEventListener("click", () =>{
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
 
-
-
-ham.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
-    ham.classList.toggle('girar');
-});
+document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click", () => {
+  hamburguer.classList.remove();
+  navMenu.classList.remove();
+}))
---
+++ sites/Seminario Web/javascript/js/scripts.js
@@ -0,0 +1,27 @@
+var premierLeague = ["Chelsea", "Man.U", "Liverpool", "Arsenal"];
+
+
+/* var nombre = prompt ('Como te llamas?');
+var pais = prompt ('De donde eres?');
+alert('Bienvenido ' + nombre + ' de '+pais); */
+
+/* var modelo = "Mustang";
+var motor = 5.5;
+var color = "azul";
+ */
+
+/* var modelo = "mustang";
+/* alert (modelo); */
+/* console.log(modelo); */ 
+
+/* var auto = {
+    nombre: "Camaro",
+    color : "Azul",
+    clasico : true,
+    anio : 1965
+}
+
+console.log(auto.nombre);
+console.log(auto.color);
+console.log(auto.clasico);
+console.log(auto.anio); */
